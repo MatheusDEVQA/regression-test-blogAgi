2732d171057336dd841199f5ecbe64131c103f5d
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-04-25 21:46:40 -0300

    Refactor to create reusable commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,32 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('clickPesquisar', () => {
+    cy.get('[aria-label="Link do ícone de pesquisa"]')
+        .click();
+})
+
+Cypress.Commands.add('validaCaixaPesquisar', () => {
+    cy.get('#ast-desktop-header .ast-dropdown-active');
+})
+
+Cypress.Commands.add('validaOcultacaoCaixaPesquisar', () => {
+    cy.get('#ast-desktop-header .ast-dropdown-active').should('not.exist');
+})
+Cypress.Commands.add('escreverPesquisa', (searchText) => {
+    cy.get('#search-field')
+        .type(`${searchText}`);
+})
+
+Cypress.Commands.add('validaTituloPesquisa', (searchText) => {
+    cy.get('#primary h1')
+        .should('contain.text', ` Resultados encontrados para: ${searchText}`);
+})
+
+Cypress.Commands.add('validaLinkArtigo', () => {
+    cy.get('#main article a')
+        .should('have.attr', 'href')
+        .and('match', /^https:\/\/blogdoagi\.com\.br\//);
+})
+
+Cypress.Commands.add('validaTextoInsucessoPesquisa', () => {
+    cy.get('#main p')
+    .should('contain.text', 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.');
+})
